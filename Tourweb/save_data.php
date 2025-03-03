d0ae22f89bed8e1e839b242d9c0d2151f8d5691a
vkaleInnovates <vijaykale.edunet@gmail.com>
2025-03-03 22:00:38 +0530

    Add files via upload

---
+++ Tourweb/save_data.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost"; // XAMPP default server
+$username = "root"; // Default MySQL username in XAMPP
+$password = ""; // No password by default
+$dbname = "contact_db"; // Your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Prepare SQL statement
+$sql = "INSERT INTO contacts (name, email, message) VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Message sent successfully!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
